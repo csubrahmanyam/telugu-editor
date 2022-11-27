9218116f4268a9d7fa394eb8a8349e4f931ea46a
C.Subrahmanyam <112937168+csubrahmanyam@users.noreply.github.com>
2022-11-27 14:13:43 +0530

    add code!

---
+++ telugu-editor/index.js
@@ -0,0 +1,37 @@
+const size = document.getElementById('size');
+const editor = document.getElementById('editor');
+var font_size_bool = false;
+var editor_color_bool = false;
+
+function size_ok() {
+    let size = document.getElementById('size_input').value;
+    editor.style.fontSize = size+"px";    
+}
+
+function font_size() {
+    if (font_size_bool === false) {
+        size.style.display = "block";
+        font_size_bool = true;
+        
+    }else
+    if (font_size_bool === true) {
+        size.style.display = "none";
+        font_size_bool = false;
+    }
+}
+function bg_btn_fun() {
+    if (editor_color_bool === false) {
+        document.getElementById('bg_options').style.display = "block";
+        editor_color_bool = true;
+    } else 
+    if (editor_color_bool === true) {
+        document.getElementById('bg_options').style.display = "none";
+        editor_color_bool = false;
+    }
+}
+function bg(color) {
+    
+    editor.style.backgroundColor = color;
+    
+}
+
